f82c4ebb4b906e98a8c774a8157234a97e39b405
Drikus Roor <drikusroor@gmail.com>
2020-03-16 16:51:41 +0100

    Add autocomplete multiselect

--- src/components/filter-form/index.js
+++ src/components/filter-form/index.js
@@ -5,20 +5,19 @@ import { connect } from 'react-redux'
 import { makeStyles, useTheme } from '@material-ui/core/styles'
 import Checkbox from '@material-ui/core/Checkbox'
 import FormControlLabel from '@material-ui/core/FormControlLabel'
-import FormControl from '@material-ui/core/FormControl'
-import Select from '@material-ui/core/Select'
-import InputLabel from '@material-ui/core/InputLabel'
 import { Grid } from '@material-ui/core'
-import Chip from '@material-ui/core/Chip'
-import Input from '@material-ui/core/Input'
-import MenuItem from '@material-ui/core/MenuItem'
 import {
   removeCountry,
   setCountryFilter,
   toggleShowPerCountry,
 } from '../../store/filters/actions'
+import Autocomplete from '@material-ui/lab/Autocomplete'
+import TextField from '@material-ui/core/TextField'
 
 const useStyles = makeStyles(theme => ({
+  container: {
+    marginBottom: theme.spacing(0.5),
+  },
   formControl: {
     margin: theme.spacing(1),
     minWidth: 240,
@@ -91,8 +90,8 @@ const FilterForm = props => {
     SHOW_PER_COUNTRY: showPerCountry,
   } = filters
 
-  const handleCountrySelectChange = event => {
-    setCountryFilter(event.target.value)
+  const handleCountrySelectChange = (event, value) => {
+    setCountryFilter(value)
   }
 
   const onDelete = (event, value) => {
@@ -110,58 +109,26 @@ const FilterForm = props => {
 
   return (
     <form onSubmit={handleSubmit}>
-      <Grid container>
+      <Grid container spacing={4} className={classes.container}>
         <Grid item md={8} lg={9}>
-          <div>
-            <FormControl className={classes.formControl}>
-              <InputLabel id="demo-mutiple-chip-label">
-                Country/Region
-              </InputLabel>
-              <Select
-                labelId="demo-mutiple-chip-label"
-                id="demo-mutiple-chip"
-                multiple
-                value={countryFilter}
-                onChange={handleCountrySelectChange}
-                input={<Input id="select-multiple-chip" />}
-                defaultValue={[]}
-                renderValue={selected => {
-                  return (
-                    <div className={classes.chips}>
-                      {selected.map(value => (
-                        <Chip
-                          key={value}
-                          color="secondary"
-                          label={value}
-                          className={classes.chip}
-                          onDelete={event => onDelete(event, value)}
-                        />
-                      ))}
-                      {selected && selected.length > 0 && (
-                        <Chip
-                          label="Clear filter"
-                          className={classes.chip}
-                          onClick={event => onDelete(event)}
-                          onDelete={event => onDelete(event)}
-                        />
-                      )}
-                    </div>
-                  )
-                }}
-                MenuProps={MenuProps}
-              >
-                {countries.map(country => (
-                  <MenuItem
-                    key={country}
-                    value={country}
-                    style={getStyles(country, countryFilter, theme)}
-                  >
-                    {country}
-                  </MenuItem>
-                ))}
-              </Select>
-            </FormControl>
-          </div>
+          <Autocomplete
+            multiple
+            id="tags-outlined"
+            options={countries}
+            getOptionLabel={option => option}
+            value={countryFilter}
+            filterSelectedOptions
+            fullWidth
+            onChange={handleCountrySelectChange}
+            renderInput={params => (
+              <TextField
+                {...params}
+                variant="outlined"
+                label="Country/Region"
+                placeholder="Select country"
+              />
+            )}
+          />
         </Grid>
         <Grid item md={4} lg={3}>
           <div>
