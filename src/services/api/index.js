e3e2a0f99d96cc9f3c5a63e2f44dc4954e477229
Drikus Roor <drikusroor@gmail.com>
2020-03-16 14:30:33 +0100

    Re-add COVID-19

--- src/services/api/index.js
+++ src/services/api/index.js
@@ -2,17 +2,17 @@ import http from '../http'
 
 const fetchConfirmed = async () =>
   http(
-    '/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv',
+    '/COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv',
   )
 
 const fetchDeaths = async () =>
   http(
-    '/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv',
+    '/COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv',
   )
 
 const fetchRecovered = async () =>
   http(
-    '/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv',
+    '/COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv',
   )
 
 export async function fetchTimeSeries() {
