3f94c8f4c372a5abd4646312da979b7cad13c4a7
Drikus Roor <drikusroor@gmail.com>
2020-04-04 12:11:54 +0200

    Add skeletons

--- src/pages/chart-page/index.js
+++ src/pages/chart-page/index.js
@@ -10,6 +10,8 @@ import {
 import { getFilterFormValues } from '../../store/form/selectors'
 import FilterForm from '../../components/filter-form'
 import { useParams } from 'react-router-dom'
+import { Grid } from '@material-ui/core'
+import Skeleton from '@material-ui/lab/Skeleton'
 
 const ChartPage = ({
   countries,
@@ -35,7 +37,44 @@ const ChartPage = ({
           />
           <DataTableTabs datasets={timeSeries} filters={filters} />
         </>
-      ) : null}
+      ) : (
+        <>
+          <Grid container spacing={2}>
+            {/* FILTER FORM */}
+            <Grid item sm="6">
+              <Skeleton variant="rect" height={60} />
+            </Grid>
+            <Grid item sm="6">
+              <Skeleton variant="rect" height={60} />
+            </Grid>
+            <Grid item sm="12">
+              <Skeleton variant="rect" height={70} />
+            </Grid>
+            <Grid item sm="6">
+              <Skeleton variant="rect" height={60} />
+            </Grid>
+            <Grid item sm="6">
+              <Skeleton variant="rect" height={60} />
+            </Grid>
+            <Grid item sm="4">
+              <Skeleton variant="rect" height={60} />
+            </Grid>
+            <Grid item sm="8"></Grid>
+
+            {/* CHART */}
+            <Grid item sm="12">
+              <Skeleton variant="rect" height={50} />
+            </Grid>
+            <Grid item sm="2">
+              <Skeleton variant="rect" height={50} />
+            </Grid>
+            <Grid item sm="10"></Grid>
+            <Grid item sm="12">
+              <Skeleton variant="rect" height={400} />
+            </Grid>
+          </Grid>
+        </>
+      )}
     </Layout>
   )
 }
